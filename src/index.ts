9aa90eec65cac5a021367d311af1124206217c77
Mert Topal <mert.topal@opinnate.com>
2026-01-07 11:45:54 +0300

    remove comment lines

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,6 @@
 import GatewayServer from '@/modules/GatewayServer/GatewayServer';
 
 const main = async () => {
-  // module.middleware('client', 0xCAFE, ctrl.HardwareID);
-  // module.middleware('remote', 0xA100, ctrl.RedirectDownload);
-  // module.middleware('remote', 0xA101, ctrl.RewriteShardList);
-  // module.middleware('remote', 0xA102, ctrl.LoginResponse);
-  // module.middleware('remote', 0x2322, ctrl.AutoCaptcha);
-
   const gatewayServer = new GatewayServer();
   gatewayServer.start();
 };
